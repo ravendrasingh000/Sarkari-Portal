169ba739954a4aad633cac25efa90b48d0eaabcc
Shalindra-coder <shalindrapaoriya23@navgurukul.org>
2026-03-23 15:34:55 +0530

    Initial commit - Sarkari Portal App

---
+++ sarkari_portal/sarkari_portal/doctype/application_details/application_details.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, shalindra  and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Application Details", {
+// 	refresh(frm) {
+
+// 	},
+// });
